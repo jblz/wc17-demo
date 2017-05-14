1bed759436935139213b8246f917501a849a364f
Jeff Bowen <jeff@automattic.com>
2017-05-14 08:49:43 -0400

    add hello-world app

---
+++ hello-world/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('wc17-demo__hello-world')
+);
