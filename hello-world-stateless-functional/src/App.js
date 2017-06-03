3bb42531c765f049672b52a5a551b2fc92888f8a
Jeff Bowen <jeff@automattic.com>
2017-06-03 14:16:29 -0400

    pull out `WhatStatelessComponentsDo` component

--- hello-world-stateless-functional/src/App.js
+++ hello-world-stateless-functional/src/App.js
@@ -1,15 +1,23 @@
 import React from 'react';
 
-function App( props ) {
+function WhatStatelessComponentsDo( props ) {
 	return (
 		<div>
-			<h1>Hello World!</h1>
-			This is a "stateless functional" component
 			<h2>They</h2>
 			<ul>
 				<li>Take props</li>
 				<li>Return a React element</li>
 			</ul>
+		</div>
+	);
+}
+
+function App( props ) {
+	return (
+		<div>
+			<h1>Hello World!</h1>
+			This is a "stateless functional" component
+			<WhatStatelessComponentsDo />
 			<h2>Passed Props</h2>
 			WordCamp: { props.wordcamp }<br />
 			Feeling: { props.feeling }
